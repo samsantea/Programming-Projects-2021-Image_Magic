a421478c9859ac1ac5a3515d04354cb06fb9a0d6
Samantha J <sam.jver@gmail.com>
2021-10-08 12:49:55 -0700

    Converted image to grayscale.

--- image_magic.py
+++ image_magic.py
@@ -7,6 +7,7 @@ from PIL import Image
 
 # Use single quotes when referring to something
 image = Image.open('./halloween-unsplash.jpg')
+output_image = Image.open('./halloween-unsplash.jpg')
 
 # Grab pixel information
 # inside parentheses must be x, y value of pixel in tuple/list
@@ -20,14 +21,30 @@ image_width = image.width
 image_height = image.height
 # Top bottom
 
+# Modify the image to convert it from colour to grayscale
+# (r, g, b) --> (?, ?, ?)
+# take the r g b, add them up and divide by 3
+# replace r, g, b with that AVERAGE value
+
 for y in range(image_height):
     # Left to right
     for x in range(image_width):
         # Grab pixel information for THIS pixel
         pixel = image.getpixel((x, y))
 
-        print(f"\nPixel location: {x}, {y}")
-        # Print pixel values
-        print(f"red: {pixel[0]}")
-        print(f"green: {pixel[1]}")
-        print(f"blue: {pixel[2]}")
+        # GRAB R, G, B
+        # red = pixel[0]
+        # green = pixel[1]
+        # blue = pixel[2]
+
+        red, green, blue = pixel # can only be done with lists or tuples
+
+        # calculate the average
+        average = sum(pixel)/len(pixel)
+        average = int( (red + green + blue) / 3 )# same as above
+        # create a gray pixel
+        gray_pixel = (average, average, average)
+        # TODO: put that in the new image
+        output_image.putpixel((x, y), gray_pixel)
+
+output_image.save('grayscale.jpg')
